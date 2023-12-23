3014ce5603b00492d363bf994507f9845455afdd
SinegubAnastasiya <fedukevich98@mail.ru>
2023-12-23 15:11:03 +0300

    added some remarks

---
+++ src/helper/validation.js
@@ -0,0 +1,21 @@
+function isValidUser(req, res, next) {
+    if (!req.body.hasOwnProperty('name')) throw new Error('There is no name in body')
+    if (!req.body.hasOwnProperty('surname')) throw new Error('There is no surname in body')
+    if (!req.body.hasOwnProperty('email')) throw new Error('There is no email in body')
+    if (!req.body.hasOwnProperty('pwd')) throw new Error('There is no pwd in body')
+    
+    const { name, surname, email, pwd } = req.body
+
+    if (!name) throw new Error('Name not found')
+    if (!surname) throw new Error('Surname not found')
+    if (!email) throw new Error('Email not found')
+    if (!pwd) throw new Error('Password not found')
+    if (!isNaN(name)) throw new Error('Name is not a string')
+    if (!isNaN(surname)) throw new Error('Surame is not a string')
+    if (!/^[\w]+@[a-z]+\.[a-z]{2,5}$/gm.test(email)) throw new Error('Email is invalid')
+    if (pwd.length < 8) throw new Error('Pwd is invalid')
+
+    next()
+}
+
+module.exports = { isValidUser }
